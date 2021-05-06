77723d2d2e1bff401497570e5a68c5f99d927a18
Thibaud Christe <thibaud.christe@he-arc.ch>
2021-05-06 21:43:31 +0200

    First http request test

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,2 +1,2 @@
 // export const BASE_URL = 'http://localhost:8080';
-export const BASE_URL = 'http://env-8336642.jcloud-ver-jpc.ik-server.com/';
+export const BASE_URL = 'http://env-8336642.jcloud-ver-jpc.ik-server.com';
