c958c8cc0358e59c6b6a4c02b576979bfb0e5168
Thibaud Christe <thibaud.christe@he-arc.ch>
2021-05-12 17:46:55 +0200

    Commit before update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CustomMaterialModule} from './custom-material-module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import {HttpClientModule} from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    CustomMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
