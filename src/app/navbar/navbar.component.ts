3400ad9204fefa54e92f1e996ac3a6ec20f136c1
Thibaud Christe <thibaud.christe@he-arc.ch>
2021-05-25 23:48:35 +0200

    Fix - affichage bon bouton

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,33 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {LoginService} from '../login/login.service';
-import {map, take} from 'rxjs/operators';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
 
-  isLogged: boolean;
+  isLogged = false;
+  private userSub: Subscription;
 
   constructor(private loginService: LoginService) { }
 
   ngOnInit(): void {
-    this.checkLoggin().subscribe(res => {
-      this.isLogged = res;
+    this.userSub = this.loginService.user.subscribe(user => {
+      this.isLogged = !!user;
     });
   }
 
-  checkLoggin() {
-    return this.loginService.user.pipe(take(1), map(user => {
-      const isAuth = !!user;
-      if (isAuth) {
-        return true;
-      }
-      return false;
-    }));
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
   }
-
-
 }
